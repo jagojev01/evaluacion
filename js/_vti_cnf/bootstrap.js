8f3a6b65b6fe89d217d7d620e834474cd4f4a7c3
Joule Martínez <152213894+jagojev01@users.noreply.github.com>
2024-08-27 14:43:12 -0600

    Add files via upload

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|27 Aug 2020 12:06:44 -0000
+vti_extenderversion:SR|12.0.0.6500
+vti_backlinkinfo:VX|blog.html contact.html about.html furniture.html index.html
---
+++ js/_vti_cnf/custom.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 Feb 2020 07:25:40 -0000
+vti_extenderversion:SR|12.0.0.6500
+vti_backlinkinfo:VX|blog.html contact.html about.html furniture.html index.html
